119d78f1a9711f51e5197ddd17af343fb3be7eb0
Black-Hawk-005 <premdanasekaran@gmail.com>
2026-06-17 20:24:20 +0530

    Added login route in auth router

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,11 +1,12 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import or_, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app import models
-from app.auth import hash_password
+from app.auth import create_access_token, hash_password, verify_password
 from app.database import get_db
 from app.schemas import UserCreate, UserRead, UserRole
 
@@ -25,7 +26,7 @@ async def register(
         select(models.Users).where(
             or_(
                 models.Users.email == user_details.email,
-                models.Users.phone_number == user_details.phone,
+                models.Users.phone == user_details.phone,
             )
         )
     )
@@ -37,7 +38,7 @@ async def register(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Email already registered",
             )
-        if existing_user.phone_number == user_details.phone:
+        if existing_user.phone == user_details.phone:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Phone number already registered",
@@ -47,7 +48,7 @@ async def register(
         role=user_details.role,
         full_name=user_details.full_name,
         email=user_details.email,
-        phone_number=user_details.phone,
+        phone=user_details.phone,
         password_hash=hash_password(user_details.password),
     )
 
@@ -56,3 +57,34 @@ async def register(
     await db.refresh(new_user)
 
     return new_user
+
+
+@router.post("/login")
+async def login(
+    request_form: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Annotated[AsyncSession, Depends(get_db)],
+):
+    result = await db.execute(
+        select(models.Users).where((models.Users.email) == request_form.username)
+    )
+    existing_user = result.scalar_one_or_none()
+    if existing_user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Check your request and try again",
+        )
+
+    # Password verification
+    if not verify_password(request_form.password, existing_user.password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Check your request and try again",
+        )
+
+    token = create_access_token(
+        {
+            "sub": str(existing_user.id),
+            "role": existing_user.role.value,
+        }
+    )
+    return {"access_token": token, "token_type": "bearer"}

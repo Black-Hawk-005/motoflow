fb0fef8c3b17755881e2b0377772e6a68eec56c4
Black-Hawk-005 <premdanasekaran@gmail.com>
2026-06-23 00:25:53 +0530

    Added vehicle details update route

--- app/routers/vehicles.py
+++ app/routers/vehicles.py
@@ -2,13 +2,13 @@ from typing import Annotated, List
 from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy import delete, select
+from sqlalchemy import and_, delete, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app import models
 from app.database import get_db
 from app.dependencies import get_current_user, require_customer
-from app.schemas import UserRole, VehicleCreate, VehicleRead
+from app.schemas import UserRead, UserRole, VehicleCreate, VehicleRead, VehicleUpdate
 
 router = APIRouter()
 
@@ -94,9 +94,7 @@ async def get_vehicle(
     return vehicle
 
 
-router.delete("/{vehicle_id}", status_code=status.HTTP_204_NO_CONTENT)
-
-
+@router.delete("/{vehicle_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_vehicle(
     vehicle_id: UUID,
     db: Annotated[AsyncSession, Depends(get_db)],
@@ -108,7 +106,7 @@ async def delete_vehicle(
     vehicle = result.scalar_one_or_none()
     if not vehicle:
         raise HTTPException(
-            status_code=status.HTTP_404_BAD_REQUEST, detail="Vehicle not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Vehicle not found"
         )
 
     if (
@@ -116,8 +114,62 @@ async def delete_vehicle(
         and vehicle.customer_id != current_user.id
     ):
         raise HTTPException(
-            status_code=status.HTTP_404_BAD_REQUEST, detail="Vehicle not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Vehicle not found"
         )
 
     await db.execute(delete(models.Vehicle).where(models.Vehicle.id == vehicle_id))
     await db.commit()
+
+
+@router.patch("/{vehicle_id}", response_model=VehicleRead)
+async def update_vehicle(
+    vehicle_id: UUID,
+    vehicle_details: VehicleUpdate,
+    db: Annotated[AsyncSession, Depends(get_db)],
+    current_user: Annotated[models.Users, Depends(get_current_user)],
+):
+    results = await db.execute(
+        select(models.Vehicle).where((models.Vehicle.id == vehicle_id))
+    )
+    vehicle = results.scalar_one_or_none()
+    if not vehicle:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Vehicle not found",
+        )
+
+    if (
+        current_user.role == UserRole.CUSTOMER
+        and vehicle.customer_id != current_user.id
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Vehicle not found",
+        )
+
+    update_data = vehicle_details.model_dump(exclude_unset=True)
+    if "license_plate" in update_data.keys():
+        result = await db.execute(
+            select(models.Vehicle).where(
+                and_(
+                    (models.Vehicle.license_plate) == update_data["license_plate"],
+                    (models.Vehicle.id) != vehicle_id,
+                )
+            )
+        )
+        existing_vehicle = result.scalar_one_or_none()
+        if existing_vehicle:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="License plate already registered",
+            )
+
+    for field, value in update_data.items():
+        setattr(vehicle, field, value)
+
+    await db.commit()
+    await db.refresh(
+        vehicle, attribute_names=["make", "model", "year", "license_plate"]
+    )
+
+    return vehicle

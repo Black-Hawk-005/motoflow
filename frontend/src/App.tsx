25d4dab808daf398d747f3a06d834a000d7d6b8d
Black-Hawk-005 <premdanasekaran@gmail.com>
2026-06-30 13:55:51 +0530

    Changed the addition of BrowserRouter and QueryClient to main.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient } from "@tanstack/react-query";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <BrowserRouter>
-        <QueryClientProvider>
-          <App />
-        </QueryClientProvider>
-      </BrowserRouter>
-    </>
-  );
+  return <>App</>;
 }
 
 export default App;

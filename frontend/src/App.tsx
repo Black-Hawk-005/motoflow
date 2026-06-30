4189446fbf34047357c674eab7a9f5162ae370b8
Black-Hawk-005 <premdanasekaran@gmail.com>
2026-06-30 22:30:49 +0530

    Created 2 routes one for login and another for dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
+import { DashboardPage } from "./pages/DashboardPage";
 import "./App.css";
-
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;

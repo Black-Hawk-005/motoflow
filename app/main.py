692430cf1484caae367188af9307fb29bbb670b2
Black-Hawk-005 <premdanasekaran@gmail.com>
2026-06-30 13:52:49 +0530

    Added CORS middleware for frontend

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import sys
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import engine
 from app.routers import (
@@ -25,7 +26,19 @@ async def lifespan(_app: FastAPI):
     await engine.dispose()
 
 
+origins = [
+    "http://localhost:5173",  # Default Vite port
+    "http://127.0.0.1:5173",
+]
+
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    middleware_class=CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(admin.router, prefix="/admin", tags=["admin"])

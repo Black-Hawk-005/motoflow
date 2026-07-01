6f65545cec9a78164e73bdbae2625ba80f4170b6
Black-Hawk-005 <premdanasekaran@gmail.com>
2026-07-01 23:29:30 +0530

    Created routes for user home page and root page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,22 @@ import { LoginPage } from "./pages/LoginPage";
 import { DashboardPage } from "./pages/DashboardPage";
 import "./App.css";
 import { PrivateRoute } from "./components/PrivateRoute";
+import { CustomerPage } from "./pages/CustomerPage";
+import { MechanicPage } from "./pages/MechanicPage";
+import { AdminPage } from "./pages/AdminPage";
+import { Navigate } from "react-router-dom";
+
 function App() {
   return (
     <>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/customer" element={<CustomerPage />} />
+          <Route path="/mechanic" element={<MechanicPage />} />
+          <Route path="/admin" element={<AdminPage />} />
         </Route>
       </Routes>
     </>

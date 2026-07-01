d5bea7df0f3fd8093d318f15561eee052aa367cc
Black-Hawk-005 <premdanasekaran@gmail.com>
2026-07-01 23:30:42 +0530

    Added the useMe hook for role wise redirection

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import { useMe } from "../hooks/useMe";
+import { useNavigate } from "react-router-dom";
+
 export const DashboardPage = () => {
+  const { data, isLoading, isError } = useMe();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!data) return;
+    if (data.role === "customer") navigate("/customer");
+    else if (data.role === "mechanic") navigate("/mechanic");
+    else navigate("/admin");
+  }, [data, navigate]);
   return (
     <>
-      <h1>Dashboard</h1>
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Something went wrong</p>}
     </>
   );
 };

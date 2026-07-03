ecd4c701ce71c9563ce64f81ac35a5239dd2f7eb
Black-Hawk-005 <premdanasekaran@gmail.com>
2026-07-04 00:00:28 +0530

    Renaming the useCreateServiceRequest hook file

--- frontend/src/hooks/useCreateServiceRequest.ts
+++ frontend/src/hooks/useCreateServiceRequest.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createServiceRequest } from "../api/serviceRequest";
 
-export const useCreateServiceRequests = () => {
+export const useCreateServiceRequest = () => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: createServiceRequest,

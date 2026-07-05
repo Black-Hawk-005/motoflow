91ab065f69c290eaee8ed8d95485d00c8d23d520
Black-Hawk-005 <premdanasekaran@gmail.com>
2026-07-05 12:24:47 +0530

    Added component to doublecheck the user role before login

---
+++ frontend/src/components/RoleRoute.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import { useMe } from "../hooks/useMe";
+import type { User } from "../types/auth";
+import { Navigate } from "react-router-dom";
+
+interface RoleRouteProps {
+  allowedRoles: User["role"][];
+}
+
+export const RoleRoute = ({ allowedRoles }: RoleRouteProps) => {
+  const { data, isLoading } = useMe();
+  return (
+    <>
+      {isLoading && <p>Loading...</p>}
+      {data && allowedRoles.includes(data?.role) && <Outlet />}
+      {!isLoading && !data && <Navigate to="/dashboard" />}
+    </>
+  );
+};

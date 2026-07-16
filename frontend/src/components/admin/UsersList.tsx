1c2b11da66a01ded9c0fec2ac6e54d73acd39675
Black-Hawk-005 <premdanasekaran@gmail.com>
2026-07-17 00:33:30 +0530

    UsersList component for AdminPage created.

---
+++ frontend/src/components/admin/UsersList.tsx
@@ -0,0 +1,53 @@
+import { useState, type SubmitEvent, useEffect } from "react";
+
+import { useUsers } from "../../hooks/admin/useUsers";
+import type { User } from "../../types/auth";
+
+export const UsersList = () => {
+  const [role, setRole] = useState<User["role"] | undefined>(undefined);
+  const roleOptions: { label: string; value: User["role"] | undefined }[] = [
+    { label: "All", value: undefined },
+    { label: "Customer", value: "customer" },
+    { label: "Mechanic", value: "mechanic" },
+    { label: "Admin", value: "admin" },
+  ];
+
+  const {
+    data: users,
+    isLoading: isUsersLoading,
+    isError: isUsersError,
+  } = useUsers(role);
+
+  return (
+    <div>
+      <h3>Users</h3>
+      {isUsersLoading && <p>Loading...</p>}
+      {roleOptions.map((r) => (
+        <button onClick={() => setRole(r.value)}>{r.label}</button>
+      ))}
+      <table>
+        <thead>
+          <tr>
+            <th>S No.</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map((user, idx) => (
+            <tr>
+              <td>{idx + 1}</td>
+              <td>{user.full_name}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+              <td>{user.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {isUsersError && <p>Unable to load users</p>}
+    </div>
+  );
+};
--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,7 +1,10 @@
+import { UsersList } from "../components/admin/UsersList";
+
 export const AdminPage = () => {
   return (
     <>
       <h1>Admin</h1>
+      <UsersList />
     </>
   );
 };

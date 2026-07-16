18f2487e2a59e54047fd22752b9448a820393139
Black-Hawk-005 <premdanasekaran@gmail.com>
2026-07-17 01:35:14 +0530

    Wiring AssignMechanicControl into ServiceRequestDetail.

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,10 +1,13 @@
 import { UsersList } from "../components/admin/UsersList";
+import { ServiceRequestList } from "../components/serviceRequest/ServiceRequestList";
 
 export const AdminPage = () => {
   return (
     <>
       <h1>Admin</h1>
       <UsersList />
+
+      <ServiceRequestList />
     </>
   );
 };
--- frontend/src/pages/ServiceRequestDetail.tsx
+++ frontend/src/pages/ServiceRequestDetail.tsx
@@ -3,12 +3,14 @@ import { useParams } from "react-router-dom";
 import { useServiceRequest } from "../hooks/serviceRequest/useServiceRequest";
 import { useMe } from "../hooks/auth/useMe";
 import { useApproveServiceRequest } from "../hooks/serviceRequest/useApproveServiceRequest";
+
 import { LineItemsList } from "../components/lineItems/LineItemsList";
 import { CommentList } from "../components/comments/CommentList";
 import { CreateCommentForm } from "../components/comments/CreateCommentForm";
 import { CreateLineItemForm } from "../components/lineItems/CreateLineItemForm";
 import { UpdateStatusControl } from "../components/serviceRequest/updateStatusControl";
 import { RejectServiceRequestApprovalForm } from "../components/serviceRequest/RejectServiceRequestApprovalForm";
+import { AssignMechanicControl } from "../components/serviceRequest/AssignMechanicControl";
 
 const ServiceRequestDetail = () => {
   const { id } = useParams<{ id: string }>();
@@ -40,16 +42,27 @@ const ServiceRequestDetail = () => {
             Approve
           </button>
         )}
+      {user?.role === "admin" && (
+        <AssignMechanicControl
+          serviceRequestId={serviceRequest?.id as string}
+        />
+      )}
+
       {user?.role === "customer" &&
         serviceRequest?.status === "action_required" && (
           <RejectServiceRequestApprovalForm id={id as string} />
         )}
+
       {isApproveSRError && <p>Failed to approve service request</p>}
+
       <UpdateStatusControl id={id as string} />
+
       <p>Initial Complaint</p>
       <p>{serviceRequest?.initial_complaint}</p>
       <p>Created at: {serviceRequest?.created_at}</p>
-
+      {user?.role === "admin" && (
+        <p>Assigned mechanic id: {serviceRequest?.mechanic_id}</p>
+      )}
       <LineItemsList id={id as string} />
       <CreateLineItemForm id={id as string} />
 
